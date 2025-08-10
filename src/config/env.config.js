c737e498681fe38ae10127919668fccb6a06e769
Sharjeel Faiq <mrmalik610@gmail.com>
2025-08-10 20:53:36 +0500

    Added envalid npm package

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,5 +1,44 @@
 import dotenv from "dotenv";
+import { cleanEnv, str, port, email, url, testOnly } from "envalid";
 
 dotenv.config();
 
-export const env = process.env;
+const validators = {
+  PORT: port({ devDefault: 3000, desc: "Port number" }),
+  NODE_ENV: str({
+    choices: ["development", "test", "production"],
+    default: "development",
+    desc: "Environment type",
+  }),
+
+  BACKEND_BASE_URL_DEV: url({ desc: "Development backend URL" }),
+  BACKEND_BASE_URL_PROD: url({ desc: "Production backend URL" }),
+  FRONTEND_BASE_URL_DEV: url({ desc: "Development frontend URL" }),
+  FRONTEND_BASE_URL_PROD: url({ desc: "Production frontend URL" }),
+
+  DATABASE_URI: url({ desc: "MongoDB connection string" }),
+  DATABASE_NAME: str({ desc: "MongoDB database name" }),
+  JWT_SECRET_KEY: str({
+    devDefault: testOnly("test-secret"),
+    desc: "JWT secret key",
+  }),
+
+  EMAIL_HOST: str({ desc: "Email host" }),
+  EMAIL_PORT: port({ desc: "Email port" }),
+  USER_EMAIL: email({ desc: "Email address" }),
+  USER_PASSWORD: str({ desc: "Email password" }),
+
+  CLOUDINARY_CLOUD_NAME: str({ desc: "Cloudinary cloud name" }),
+  CLOUDINARY_API_KEY: str({ desc: "Cloudinary API key" }),
+  CLOUDINARY_API_SECRET: str({ desc: "Cloudinary API secret" }),
+};
+
+export const env = cleanEnv(process.env, validators, {
+  reporter: ({ errors }) => {
+    const invalidVars = Object.keys(errors);
+    if (invalidVars.length > 0) {
+      console.error("Invalid ENV variables:", invalidVars);
+      process.exit(1);
+    }
+  },
+});
