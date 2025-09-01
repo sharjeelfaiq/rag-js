60be35066fee4faf62f6aa81ab7ab32c8b9375f1
Sharjeel Faiq <mrmalik610@gmail.com>
2025-09-01 14:01:03 +0500

    code refactored

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ import {
   otpRoutes,
   userRoutes,
 } from "#modules/index.js";
-import { validateMiddleware } from "#middleware/index.js";
+import { validateMiddleware as validate } from "#middleware/index.js";
 
 // Parent router
 const appRouter = express.Router();
@@ -24,12 +24,8 @@ appRouter.use("/api/v1", v1Router);
 // V1 routes
 v1Router.use("/auth", authRoutes);
 v1Router.use("/email", emailRoutes);
-v1Router.use(
-  "/notifications",
-  validateMiddleware.accessToken,
-  notificationRoutes
-);
+v1Router.use("/notifications", validate.accessToken, notificationRoutes);
 v1Router.use("/otp", otpRoutes);
-v1Router.use("/users", validateMiddleware.accessToken, userRoutes);
+v1Router.use("/users", validate.accessToken, userRoutes);
 
 export default appRouter;
